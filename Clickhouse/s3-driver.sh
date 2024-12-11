d3a20a15e29bf15e43d6b074fc2cd55623d44f2d
gowtham-at-302716195582 <nookala3656@gmail.com>
2024-12-11 08:17:05 +0530

    added the clickhouse

--- Clickhouse/s3-driver.sh
+++
@@ -1,7 +0,0 @@
-wget https://github.com/awslabs/mountpoint-s3-csi-driver/releases/download/helm-chart-aws-mountpoint-s3-csi-driver-1.11.0/aws-mountpoint-s3-csi-driver-1.11.0.tgz
-
-tar zxvf aws-mountpoint-s3-csi-driver-1.11.0.tgz
-
-cd aws-mountpoint-s3-csi-driver
-
-helm install as-s3-driver . --values values.yaml -n kube-system --debug
